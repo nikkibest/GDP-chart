f07f179ebaf8668f51e3fbe91d124945c6425615
Niklas Simonsen <nsimonsen@grundfos.com>
2023-01-29 23:41:29 +0100

    Started on d3 with initial js under control

--- script.js
+++ script.js
@@ -13,84 +13,69 @@
 const url = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json';
 const widthOfSvg = 2000;
 const heightOfSvg = 300;
+const padding = 60;
 let url_data = []
 
-// document.addEventListener('DOMContentLoaded', function(){
-  
-// });
-getData();
-
 async function getData() {
-  const response = await fetch(url);
-  const data = await response.json();
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    // Add title and description
+    document.getElementsByClassName("title")[0].innerHTML = data["name"];
+    document.getElementsByClassName("supbar")[0].innerHTML = data["description"];
 
-  document.getElementById("title").innerText = data["name"];
-  document.getElementById("supbar").innerText = data["description"];
-  console.log(data)
+    // Calculate parameters for the d3 element
+    const widthOfSvgBar = Math.ceil(widthOfSvg / data.data.length);
+    const dates = data.data.map((d) => new Date(d[0][0]))
+    console.log(dates)
+    console.log([d3.min(dates, d => d), d3.max(dates, d => d)])
+
+
+
+    const xScale = d3.scaleBand()
+      .domain(dates.map((d) => d))
+      .range([padding, widthOfSvg-padding])
+
+    const yScale = d3.scaleLinear()
+      .domain([d3.min(data.data, (d) => d[1]), d3.max(data.data, (d) => d[1])])
+      .range([heightOfSvg - padding, padding])
+
+    const xAxis = d3.axisBottom(xScale)
+    const yAxis = d3.axisLeft(yScale)
+
+    console.log((data.data[0][0]))
+    console.log(xScale(data.data[0][0]))
+    console.log((data.data[0][1]))
+    console.log(yScale(data.data[0][1]))
+
+    // Create d3 element and add data to it
+    const chart = d3.select(".chart")
+    .append("svg")
+    .attr("width", widthOfSvg)
+    .attr("height", heightOfSvg)
+
+    chart.selectAll("rect")
+    .data(data.data)
+    .enter()
+    .append("rect")
+    .attr("x", (d,i) => i * 10)
+    .attr("y", d => yScale(d[1]))//heightOfSvg - d[1]/5)
+    .attr("width", widthOfSvgBar)
+    .attr("height", d => d[1]*30)
+    .attr("fill", "white")
+    .attr("class", "bar")
+
+    chart.append("g")
+      .attr("transform", "translate(0," + (heightOfSvg - padding) + ")")
+      .call(xAxis)
+
+    chart.append("g")
+    .attr("transform", "translate(" + (padding) + ",0)")
+    .call(yAxis)
+
+    //console.log(data.data)
+  } catch (err) {
+    console.log(err)
+  }
 }
-
-// const response = fetch(url) 
-//   .then((response) => response.json())
-//   .then((data) => {
-//     url_data = data;
-//   });
-
-
-
-// console.log(response)
-// console.log(url_data)
-// let data = jsonData.data;
-// console.log(data[1][1]);
-
-
-// const widthOfSvgBar = Math.ceil(widthOfSvg / data.length);
-// console.log(widthOfSvgBar);
-
-// // This is how d3 lets you update the document
-// d3.select(".title")
-//   .append("text")
-//   .text(jsonData.name);
-
-// // This is how d3 lets you update the document
-// d3.select(".undertitle")
-//   .append("text")
-//   .text(jsonData.description);
-
-// const chart = d3.select(".chart")
-//   .append("svg")
-//   .attr("width", widthOfSvg)
-//   .attr("height", heightOfSvg);
-
-//   chart.selectAll("rect")
-//   .data(data)
-//   .enter()
-//   .append("rect")
-//   .attr("x", (d,i) => i * 10)
-//   .attr("y", d => heightOfSvg-d[1]/30)
-//   .attr("width", widthOfSvgBar)
-//   .attr("height", d => d[1])
-
-  
-// svg.selectAll("rect")
-//   .data(dataset)
-//   .enter()
-//   .append("rect")
-//   .attr("x", (d, i) => i * 30)
-//   .attr("y", (d, i) => h - 3 * d)
-//   .attr("width", 25)
-//   .attr("height", (d, i) => d * 3)
-//   .attr("fill", "navy")
-//   .attr("class", "bar")
-// // Add your code below this line
-//   .append("title")
-//   .text(d => d)
-
-// // Add your code above this line
-
-// svg.selectAll("text")
-//   .data(dataset)
-//   .enter()
-//   .append("text")
-//   .text((d) => d)
-//   .attr("x", (d, i) => i * 30)
-//   .attr("y", (d, i) => h - (d * 3 + 3))
+getData();
